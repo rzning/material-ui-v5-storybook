a77ad58abad36c0dbc2abe9335cb972eceb1963f
Zhenning Ruan <rzning@outlook.com>
2021-03-12 01:59:04 +0800

    📦 安装 Material-UI 依赖包

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@material-ui/core'
+import { AcUnit } from '@material-ui/icons'
+
 export default function App() {
   return (
     <div className="app">
       <header>
         <p>Material-UI Storybook</p>
+        <Button variant="contained" startIcon={<AcUnit />}>
+          Primary
+        </Button>
       </header>
     </div>
   )
